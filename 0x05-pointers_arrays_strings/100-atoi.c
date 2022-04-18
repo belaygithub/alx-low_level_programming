cbb9ed2306c18519d532af9f281e389e440ff8b7
belaygit <belayeshetu2014@gmail.com>
2022-04-18 07:18:40 -0700

    a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,15 @@
 #include "main.h"
 /**
+ * is_numerical - check if it is a digit
+ * @n: number
+ * Return: if a number, return 1 else return 0
+ */
+int is_numerical(unsigned int n)
+{
+  return (n >= '0' && n <= '9');
+}
+
+/**
  * _atoi - convert a string to an integer
  * @s: string
  * Return: num
