85f8c7d33b7f3d7dcdcf40d57f050efd14ba24cd
belaygit <belayeshetu2014@gmail.com>
2022-05-09 08:51:03 -0700

    a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - dog attributes
  * @name: the name of the dog
@@ -13,13 +13,14 @@ char *name;
 float age;
 char *owner;
 };
+
 /**
- * dog_t - typedef for dog structure
+ * dog - typedef for dog structure
  */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog-t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
