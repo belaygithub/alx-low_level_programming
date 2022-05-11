55885e3940b4e9edcf87cc7bae150a44e1bc60b3
belaygit <belayeshetu2014@gmail.com>
2022-05-11 02:33:27 -0700

    a function that prints a name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,5 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-/*
- * File: function_pointers.h
- * Auth: Belay Eshetu
- * Desc: Header file containing prototypes for all functions used in this porject
- */
 #include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
