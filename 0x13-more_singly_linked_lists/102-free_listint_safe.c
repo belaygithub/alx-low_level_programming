2db23a1ae2da377660433a99768f41c0f7b65f04
belaygit <belayeshetu2014@gmail.com>
2022-05-24 02:00:29 -0700

    updated 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,27 +1,61 @@
 #include "lists.h"
+#include <stdlib.h>
 /**
- * free_listint_safe - free a `listint_t` list and set the head to null
- * @h: double pointer to head of linked list
- * Description: This function should work for circular lists
- * Only loop through the list once
- * Return: size of the list that was free'd
+ * find_listint_loop_fl - finds a loop in a linked list
+ *
+ * @head: linked list to search
+ *
+ * Return: address of node where loop starts/returns, NULL if no loop
+ */
+listint_t *find_listint_loop_fl(listint_t *head)
+{
+listint_t *ptr, *end;
+if (head == NULL)
+return (NULL);
+for (end = head->next; end != NULL; end = end->next)
+{
+if (end == end->next)
+return (end);
+for (ptr = head; ptr != end; ptr = ptr->next)
+if (ptr == end->next)
+return (end->next);
+}
+return (NULL);
+}
+/**
+ * free_listint_safe - frees a listint list, even if it has a loop
+ *
+ * @h: head of list
+ *
+ * Return: number of nodes freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-listint_t *current, *hold;
-size_t count;
+listint_t *next, *loopnode;
+size_t len;
+int loop = 1;
 
-count = 0;
-current = *h;
-while (current != NULL)
+if (h == NULL || *h == NULL)
+return (0);
+loopnode = find_listint_loop_fl(*h);
+for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
 {
-count++
-hold = current;
-current = current->next;
-free(hold);
-if (hold < current)
+len++;
+next = (*h)->next;
+if (*h == loopnode && loop)
+{
+if (loopnode == loopnode->next)
+{
+free(*h);
 break;
 }
+len++;
+next = next->next;
+free((*h)->next);
+loop = 0;
+}
+free(*h);
+}
 *h = NULL;
-return (count);
+return (len);
 }
