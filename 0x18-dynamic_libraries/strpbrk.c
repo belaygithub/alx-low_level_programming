fc6e5a8798a9503d151559d77901db0950bc9ed1
belaygit <belayeshetu2014@gmail.com>
2022-07-11 11:10:28 -0700

    fixed libdynamic.so

--- 0x18-dynamic_libraries/strpbrk.c
+++ 0x18-dynamic_libraries/strpbrk.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_strpbrk - Search a string for any of a set of bytes.
+ * _strpbrk - Search a string for any of a set of bytes.
  * @s: string
  * @accept: string to match
  * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
