03f9f554f85375514ca7a1b6b1ee57105c550f76
belaygit <belayeshetu2014@gmail.com>
2022-07-11 13:38:21 -0700

    add 100-operations.so

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - the firt whole int number in a string.
+ * @s: string to convert to int
+ *
+ * Return: INT .
+ */
+int _atoi(char *s __attribute__((unused)))
+{
+return (0);
+}
