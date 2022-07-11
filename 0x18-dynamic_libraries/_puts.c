3f75be6da580ff046411628be9d473f191204e9e
belaygit <belayeshetu2014@gmail.com>
2022-07-11 11:24:08 -0700

    added _puts.c

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _puts - writes the character c to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _puts(char *s)
+{
+}
